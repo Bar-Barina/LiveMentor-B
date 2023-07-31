b86a2b071fb6345f8906933226ab6ae91614cbe4
Bar-Barina <itsbarbarina@gmail.com>
2023-07-31 22:46:10 +0300

    added crud functions to controller

--- api/codeblock/codeblock.controller.js
+++ api/codeblock/codeblock.controller.js
@@ -13,6 +13,70 @@ async function getCodeblocks(req, res) {
   }
 }
 
+async function getCodeblockById(req, res) {
+  try {
+    const codeblockId = req.params.id
+    const codeblock = await codeblockService.getById(codeblockId)
+    res.json(codeblock)
+  } catch (err) {
+    logger.error('Failed to get codeblock', err)
+    res.status(500).send({ err: 'Failed to get codeblock' })
+  }
+}
+
+async function getBrands(req, res) {
+  try {
+    const brands = await codeblockService.getBrands()
+    res.json(brands)
+  } catch (err) {
+    logger.error('Failed to get ctgs', err)
+    res.status(500).send({ err: 'Failed to get ctgs' })
+  }
+}
+
+async function addCodeblock(req, res) {
+  // const {loggedinUser} = req
+
+  try {
+    const codeblock = req.body
+    // codeblock.owner = loggedinUser._id
+    const addedCodeblock = await codeblockService.add(codeblock)
+    res.json(addedCodeblock)
+  } catch (err) {
+    logger.error('Failed to add codeblock', err)
+    res.status(500).send({ err: 'Failed to add codeblock' })
+  }
+}
+
+
+async function updateCodeblock(req, res) {
+  try {
+    const codeblock = req.body
+    const updatedCodeblock = await codeblockService.update(codeblock)
+    res.json(updatedCodeblock)
+  } catch (err) {
+    logger.error('Failed to update codeblock', err)
+    res.status(500).send({ err: 'Failed to update codeblock' })
+
+  }
+}
+
+async function removeCodeblock(req, res) {
+  try {
+    const codeblockId = req.params.id
+    const removedId = await codeblockService.remove(codeblockId)
+    res.send(removedId)
+  } catch (err) {
+    logger.error('Failed to remove codeblock', err)
+    res.status(500).send({ err: 'Failed to remove codeblock' })
+  }
+}
+
 module.exports = {
   getCodeblocks,
+  getCodeblockById,
+  addCodeblock,
+  updateCodeblock,
+  removeCodeblock,
+  getBrands
 };

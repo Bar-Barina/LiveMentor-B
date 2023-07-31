ad56b87fb68534ae3baed38040abab26835de1fe
Bar-Barina <itsbarbarina@gmail.com>
2023-07-31 22:44:02 +0300

    added crud routes in backend

--- api/codeblock/codeblock.routes.js
+++ api/codeblock/codeblock.routes.js
@@ -1,8 +1,18 @@
 const express = require("express");
 const { log } = require("../../middlewares/logger.middleware");
-const { getCodeblocks } = require("./codeblock.controller");
+const {
+  getCodeblocks,
+  getCodeblockById,
+  addCodeblock,
+  updateCodeblock,
+  removeCodeblock,
+} = require("./codeblock.controller");
 const router = express.Router();
 
 router.get("/", log, getCodeblocks);
+router.get("/:id", getCodeblockById);
+router.post("/", addCodeblock);
+router.put("/:id", updateCodeblock);
+router.delete("/:id", removeCodeblock);
 
 module.exports = router;
